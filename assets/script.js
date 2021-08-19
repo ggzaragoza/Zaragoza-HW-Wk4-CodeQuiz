639549ee05b4f1bc0066d7d59095434ff85cdb20
Greg Zaragoza <greg.g.zaragoza@gmail.com>
2021-08-18 23:59:20 -0500

    Tested function to build quiz questions

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,16 @@
 var timerEl = document.querySelector(".timer");
-var beginCountdown = document.querySelector('#generate');
+var beginQuiz = document.querySelector('#begin');
+var quiz = document.getElementById('quiz');
+// var question = document.querySelector("#question");
+// var choiceA = document.querySelector("#choiceA");
+// var choiceB = document.querySelector("#choiceB");
+// var choiceC = document.querySelector("#choiceC");
+// var choiceD = document.querySelector("#choiceD");
 
 function countdown() {
     var secondsLeft = 300;
-  
-    // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-    var timeInterval = setInterval(function () {
+
+    var timeInterval = setInterval(function() {
       secondsLeft--;
       timerEl.textContent = secondsLeft;
   
@@ -15,4 +20,56 @@ function countdown() {
     }, 1000);
 }
 
-beginCountdown.addEventListener("click", countdown);
+const question1 = {
+    ask: "This is the question?",
+    choiceA: "This answer is right",
+    choiceB: "Or maybe this one",
+    choiceC: "This is def wrong",
+    choiceD: "Who knows"
+}
+
+const question2 = {
+    ask: "This is the second question?",
+    choiceA: "A could be wrong now",
+    choiceB: "We never can tell about B",
+    choiceC: "Perhaps C is right this time",
+    choiceD: "I'm hungry"
+}
+
+var allQuestions = [question1, question2];
+
+function buildQuestion(input) {
+    var question = document.createElement("h2");
+    question.textContent = input.ask;
+    quiz.appendChild(question);
+
+    var choice1 = document.createElement("button");
+    choice1.textContent = input.choiceA;
+    quiz.appendChild(choice1);
+
+    var choice2 = document.createElement("button");
+    choice2.textContent = input.choiceB;
+    quiz.appendChild(choice2);
+
+    var choice3 = document.createElement("button");
+    choice3.textContent = input.choiceC;
+    quiz.appendChild(choice3);
+
+    var choice4 = document.createElement("button");
+    choice4.textContent = input.choiceD;
+    quiz.appendChild(choice4);
+
+}
+
+function askQuestion() {
+    buildQuestion(question1);
+    // buildQuestion(allQuestions[i]);
+
+    // for (var i = 0; i < allQuestions.length; i++) {
+
+    // }
+
+}
+
+beginQuiz.addEventListener("click", countdown);
+beginQuiz.addEventListener("click", askQuestion);
