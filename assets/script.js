0979e3bc5dee60c30c92e4f1301170a268229147
Greg Zaragoza <greg.g.zaragoza@gmail.com>
2021-08-20 10:37:50 -0500

    More work on script.js

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 var timerEl = document.querySelector(".timer");
 var beginQuiz = document.querySelector('#begin');
 var quiz = document.getElementById('quiz');
+var title = document.getElementById('titlescreen');
 // var question = document.querySelector("#question");
 // var choiceA = document.querySelector("#choiceA");
 // var choiceB = document.querySelector("#choiceB");
@@ -50,27 +51,33 @@ const allQuestions = [
     },
 ]
 
+var question = document.createElement("h2");
+var choice1 = document.createElement("button");
+var choice2 = document.createElement("button");
+var choice3 = document.createElement("button");
+var choice4 = document.createElement("button");
+
 function buildQuestion(input) {
 
     // for (var i = 0; i < allQuestions.length; i++)
 
-    var question = document.createElement("h2");
+    // var question = document.createElement("h2");
     question.innerHTML = allQuestions[input].ask;
     quiz.appendChild(question);
 
-    var choice1 = document.createElement("button");
+    // var choice1 = document.createElement("button");
     choice1.textContent = allQuestions[input].choices[0].text;
     quiz.appendChild(choice1);
 
-    var choice2 = document.createElement("button");
+    // var choice2 = document.createElement("button");
     choice2.textContent = allQuestions[input].choices[1].text;
     quiz.appendChild(choice2);
 
-    var choice3 = document.createElement("button");
+    // var choice3 = document.createElement("button");
     choice3.textContent = allQuestions[input].choices[2].text;
     quiz.appendChild(choice3);
 
-    var choice4 = document.createElement("button");
+    // var choice4 = document.createElement("button");
     choice4.textContent = allQuestions[input].choices[3].text;
     quiz.appendChild(choice4);
 
@@ -82,17 +89,74 @@ var questionNumber = 0;
 function askQuestion() {
     buildQuestion(questionNumber);
 
-    var buttons = quiz.getElementsByTagName("button");
-    buttons.addEventListener("click", questionNumber++);
+    questionNumber++;
+
+    // if (questionNumber > 0) {
+
+    // }
+}
+
+// function nextQuestion() {
+
+// var buttons = quiz.getElementsByTagName("button");
+// buttons.classList.add("answer-button");
+// var answerButtons = quiz.getElementsByClassName("answer-button");
+// answerButtons.addEventListener("click", nextQuestion);
+
+//     questionNumber++;
+
+// }
+
+
+// function removeTitle() {
+//     title.classList.add("hidden");
+// }
 
 beginQuiz.addEventListener("click", countdown);
 beginQuiz.addEventListener("click", askQuestion);
+// beginQuiz.addEventListener("click", removeTitle);
 
 
 
 
 
 
+
+
+
+// var buttons = quiz.getElementsByTagName("button");
+// buttons.classList.add("answer-button");
+// var answerButtons = quiz.getElementsByClassName("answer-button");
+// answerButtons.addEventListener("click", questionNumber++);
+
+// // buttonEl.addEventListener("click", nextQuestion);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var buttons = quiz.getElementsByTagName("button");
+// buttons.addEventListener("click", questionNumber++);
 
 
 
@@ -104,11 +168,6 @@ beginQuiz.addEventListener("click", askQuestion);
 // .setAttribute('class', 'quiz-btn')
 
 
-    // function nextQuestion() {
-    //     questionNumber++;
-    // }
-
-}
 
 // if (buttons.allQuestions.choices.correct === true) {
 //     questionNumber++;
@@ -130,6 +189,18 @@ beginQuiz.addEventListener("click", askQuestion);
 
 // buttonEl.addEventListener("click", nextQuestion);
 
+// function nextQuestion() {
+//     questionNumber++;
+//     askQuestion();
+// }
+
+
+
+
+
+
+
+
 
 // buildQuestion(allQuestions[i]);
 // for (var i = 0; i < allQuestions.length; i++) {
@@ -141,31 +212,3 @@ beginQuiz.addEventListener("click", askQuestion);
 // }
 
 
-
-
-
-
-
-
-
-
-
-
-
-// const question1 = {
-//     ask: "This is the question?",
-//     choiceA: "This answer is right",
-//     choiceB: "Or maybe this one",
-//     choiceC: "This is def wrong",
-//     choiceD: "Who knows"
-// }
-
-// const question2 = {
-//     ask: "This is the second question?",
-//     choiceA: "A could be wrong now",
-//     choiceB: "We never can tell about B",
-//     choiceC: "Perhaps C is right this time",
-//     choiceD: "I'm hungry"
-// }
-
-// var allQuestions = [question1, question2];
